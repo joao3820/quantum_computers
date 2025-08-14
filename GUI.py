555f1dba51234d8bce5e7de7c391b2bb63a2466e
MSI-JMSS\joaom <joao.miguelsilva@live.com.pt>
2025-08-14 15:37:49 +0100

    GUI template-code added

--- GUI.py
+++ GUI.py
@@ -1,3 +1,38 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import random as rd
+import sys
+import os
+from PyQt5 import QtWidgets, uic
+from tunneling_game.tunneling import *
+
+"""
+Template for GUI
+"""
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        """
+        Represents the main-window of the gui.
+        It loads the ui-file.
+        """
+        super().__init__()
+        uic.loadUi(
+            os.path.dirname(__file__)+"//widget.ui",
+            self,
+        )
+
+        self.runButton.pressed.connect(self.run_circuit)
+        self.show()
+    
+    def run_circuit(self):
+        """Run the quantum circuit, based on the API key."""
+
+        api_key = str(self.apiKey.text())  # if it's a value box, .value() instead...
+
+        return
+    
+if __name__ == '__main__':
+    qapp = QtWidgets.QApplication.instance()
+    if not qapp:
+        qapp = QtWidgets.QApplication(sys.argv)
+    
+    main_window = MainWindow()
+    qapp.exec()
