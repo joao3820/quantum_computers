eb988d06af57c92646fcd0f570e2426e40c115a2
MSI-JMSS\joaom <joao.miguelsilva@live.com.pt>
2025-08-20 22:31:18 +0100

    Minor corrections

--- README.md
+++ README.md
@@ -13,5 +13,3 @@ We would also recreate popular algorithms and papers. Currently, Qiskit also has
 - Boson Sampling Problem algorithm
 - Solving linear systems of equations
 - etc...
-
-Somthing to note about quantum algorithms is that we use a lot the word "circuit", which is weird, because it would normally refer to something ciclic, but not in this case. Here, the information travels from left to right on our schematics. To a "black box" we call "oracle". Also, when entanglement exists, we call that unit e-bit. And a quantum register is a system comprising multiple qubits.
--- tunneling_game/circuit_layout.py
+++ tunneling_game/circuit_layout.py
@@ -36,6 +36,7 @@ class CircuitLayout:
             self,
             location: int,
             controller_qubit: int,
+            second_controller_qubit = None,
     ):
         """
         Add Controlled-NOT Gate, quivalent to the classical XOR gate.
@@ -43,8 +44,12 @@ class CircuitLayout:
         Args:
             location: Integer with the qubit to be affected.
             controller_qubit: Integer with the controller qubit (only if it's =1 will this gate act).
+            second_controller_qubit: Optional integer with a second controller qubit, making an AND with the first one.
         """
-        self.qc.cx(controller_qubit, location)
+        if type(second_controller_qubit) is int:
+            self.qc.ccx(controller_qubit, second_controller_qubit, location)  # also called a "Toffoli" gate
+        else:
+            self.qc.cx(controller_qubit, location)
     
     def add_hadamard_gate(
             self,
@@ -55,20 +60,23 @@ class CircuitLayout:
 
         Args:
             locations: List of the qubits to be affected.
+
+        Note that it does NOT create entanglement. To those objects we call e-bits.
         """
         self.qc.h(locations)
 
 
 if __name__ == '__main__':
-    # Example of an half adder
+    # Example of a register (system of multiple qubits) creating an half adder
     half_adder = CircuitLayout(4, 2)
     half_adder.add_not_gate([0, 1])  # let's make both q0=1 and q1=1
     half_adder.qc.barrier()  # can be used in qiskit to make circuits easier to read
 
     half_adder.add_cnot_gate(2, 0)
     half_adder.add_cnot_gate(2, 1)
-    half_adder.qc.ccx(0,1,3)  # also called a "Toffoli" gate, q0 and q1 are both controller qubits
+    half_adder.add_cnot_gate(3, 0, 1)
 
     # extract outputs
     half_adder.qc.measure(2,0) # extract XOR value
     half_adder.qc.measure(3,1) # extract AND value
+    # Many quantum circuits can also be in "black boxes" that we call "orcales"
